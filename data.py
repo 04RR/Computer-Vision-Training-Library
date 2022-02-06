164256b867086290961eb56ad7c3f215965d0dbd
Rohit Rajesh <rajesh.rohit04@gmail.com>
2022-02-06 16:49:02 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
 # file that scans for data from ordered folders and generates DataLoader class.
 # -----------------------------------------------------------------------------------------------
-# Classification - data/train/images, data/train/data.csv, data/val/images and data/val/data.csv.
-# Segmentation - data/train/images, data/train/masks, data/val/images and data/val/masks.
-# Object Detection - data/train/images, data/train/data.csv, data/val/images and data/val/data.csv.
+# Classification - data/train/images, data/train/train_data.csv, data/val/images and data/val/val_data.csv.
+# Segmentation - data/train/images, data/train/masks, data/train/train_data.csv, data/val/images, data/val/masks and data/val/val_data.csv.
+# Object Detection - data/train/images, data/train/train_data.csv, data/val/images and data/val/val_data.csv.
 # bboxes - [x, y, h, w, classes]
 
 from torch.utils.data import Dataset
@@ -23,8 +23,8 @@ class ImageDataset(Dataset):
 
         self.transforms, self.mode, self.device = transforms, mode, device
         self.path = path
-        
-        self.df = pd.read_csv(f"{self.path}\\data.csv")
+
+        self.df = pd.read_csv(f"{self.path}\\data.csv")[:1]
 
         if mode == "classification":
             self.classes = self.df["class"].unique()
@@ -46,7 +46,8 @@ class ImageDataset(Dataset):
 
             if self.transforms:
 
-                img = self.transforms["image"]
+                transformed = self.transforms(image=img)
+                img = transformed["image"]
 
         elif self.mode == "detection":
 
@@ -54,20 +55,22 @@ class ImageDataset(Dataset):
 
             if self.transforms:
 
-                img = self.transforms["image"]
-                label = self.transforms["bboxes"]
+                transformed = self.transforms(image=img, bboxes=label)
+                img = transformed["image"]
+                label = transformed["bboxes"]
 
             label = torch.tensor(label)
 
         elif self.mode == "segmentation":
 
-            img_path = f"{self.path}/masks/" + self.df["mask_path"].iloc[idx]
+            img_path = f"{self.path}\\masks\\" + self.df["mask_path"].iloc[idx]
             label = np.array(Image.open(img_path).convert("RGB"))
 
             if self.transforms:
 
-                img = self.transforms["image"]
-                label = self.transforms["mask"]
+                transformed = self.transforms(image=img, mask=label)
+                img = transformed["image"]
+                label = transformed["mask"]
 
             label = torch.tensor(label).permute(2, 0, 1)
 
@@ -80,10 +83,12 @@ class ImageDataset(Dataset):
         return len(self.df)
 
 
-def get_dataset(path, mode, transforms= None):
+def get_dataset(path, mode, transforms=None):
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    trainset = ImageDataset(f"{path}\\train\\", mode, device, transforms= transforms, train=True)
+    trainset = ImageDataset(
+        f"{path}train\\", mode, device, transforms=transforms, train=True
+    )
     valset = ImageDataset(f"{path}\\val\\", mode, device, train=False)
 
     return trainset, valset
