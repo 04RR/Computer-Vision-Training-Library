799467b2206f00f9aeb034aab34da05c4e536b0d
Rohit Rajesh <rajesh.rohit04@gmail.com>
2022-04-17 22:37:53 +0530

    added trainer class

--- src/subpixel/train.py
+++ src/subpixel/train.py
@@ -1,6 +1,7 @@
 import torch
 from tqdm import tqdm
 import warnings
+from subpixel.data import ImageDataset
 import numpy as np
 import torch.nn as nn
 
@@ -12,121 +13,174 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 def accuracy(out, labels):
-    
-    c=0
-    
+
+    c = 0
+
     preds = torch.round(out)
     preds = preds.detach().cpu().numpy().tolist()
     labels = labels.cpu().numpy().tolist()
-    
+
     for label, pred in zip(labels, preds):
         if pred == label:
-            c+=1
+            c += 1
 
-    return c/len(out)
+    return c / len(out)
 
 
-def fit(
-    model,
-    trainset,
-    valset,
-    epochs,
-    mode,
-    loss_fn,
-    learning_rate,
-    weight_decay,
-    model_save_path,
-):
+class Trainer:
+    def __init__(
+        self,
+        model,
+        trainset,
+        valset=None,
+        epochs=10,
+        mode="classification",
+        loss_fn=nn.MSELoss(),
+        learning_rate=None,
+        weight_decay=1e-5,
+        model_save_path="./",
+    ):
+        self.model = model
+        self.trainset = trainset
+        self.valset = valset
+        self.epochs = epochs
+        self.mode = mode
+        self.loss_fn = loss_fn
+        self.weight_decay = weight_decay
+        self.model_save_path = model_save_path
 
-    optimizer = torch.optim.Adam(
-        model.parameters(), lr=learning_rate, weight_decay=weight_decay,
-    )
-    scaler = torch.cuda.amp.GradScaler()
-    losses = {"train": [], "val": []}
-    acc = {"train": [], "val": []}
+        if learning_rate != None:
+            learning_rate = self.find_lr()  # add FindLR class from utils after done
 
-    for epoch in range(epochs):
+    def fit(self):
 
-        epoch_loss = {"train": [], "val": []}
-        epoch_acc = {"train": [], "val": []}
+        flag = self.mode == "classification" or self.mode == "detection"
 
-        model.train()
-        for img, label in tqdm(trainset):
+        optimizer = torch.optim.Adam(
+            self.model.parameters(),
+            lr=self.learning_rate,
+            weight_decay=self.weight_decay,
+        )
+        scaler = torch.cuda.amp.GradScaler()
+        losses = {"train": [], "val": []}
+        acc = {"train": [], "val": []}
 
-            img, label = img.unsqueeze(0), label.unsqueeze(0)
+        for epoch in range(self.epochs):
 
-            with torch.cuda.amp.autocast():
+            epoch_loss = {"train": [], "val": []}
+            epoch_acc = {"train": [], "val": []}
 
-                pred = model(img)
-                loss = loss_fn(pred, label)
+            self.model.train()
+            for img, label in tqdm(self.trainset):
 
-                epoch_loss["train"].append(loss)
+                img, label = img.unsqueeze(0), label.unsqueeze(0)
 
-                if mode == "classification":
-                    a = accuracy(pred, label)
-                    epoch_acc["train"].append(a)
+                with torch.cuda.amp.autocast():
 
-                elif mode == "detection":
-                    a = accuracy(pred[1:5], label[1:5])
-                    epoch_acc["train"].append(a)
+                    pred = self.model(img)
+                    loss = self.loss_fn(pred, label)
 
-            scaler.scale(loss).backward()
-            scaler.step(optimizer)
-            scaler.update()
-            optimizer.zero_grad()
+                    epoch_loss["train"].append(loss)
 
-        model.eval()
-        for img, label in tqdm(valset):
+                    if self.mode == "classification":
+                        a = accuracy(pred, label)
+                        epoch_acc["train"].append(a)
 
-            img, label = img.unsqueeze(0), label.unsqueeze(0)
+                    elif self.mode == "detection":
+                        a = accuracy(pred[1:5], label[1:5])
+                        epoch_acc["train"].append(a)
 
-            with torch.cuda.amp.autocast():
+                scaler.scale(loss).backward()
+                scaler.step(optimizer)
+                scaler.update()
+                optimizer.zero_grad()
 
-                pred = model(img)
-                loss = loss_fn(pred, label)
+            losses["train"].append(sum(epoch_loss["train"]) / len(epoch_loss["train"]))
 
-                epoch_loss["val"].append(loss)
+            if self.valset != None:
 
-                if mode == "classification":
-                    a = accuracy(pred, label)
-                    epoch_acc["val"].append(a)
+                self.model.eval()
+                for img, label in tqdm(self.valset):
 
-                elif mode == "detection":
-                    a = accuracy(pred[1:5], label[1:5])
-                    epoch_acc["val"].append(a)
+                    img, label = img.unsqueeze(0), label.unsqueeze(0)
 
-        losses["val"].append(sum(epoch_loss["val"]) / len(epoch_loss["val"]))
-        losses["train"].append(sum(epoch_loss["train"]) / len(epoch_loss["train"]))
+                    with torch.cuda.amp.autocast():
 
-        if mode == "classification" or mode == "detection":
+                        pred = self.model(img)
+                        loss = self.loss_fn(pred, label)
 
-            acc["val"].append(sum(epoch_acc["val"]) / len(epoch_acc["val"]))
-            acc["train"].append(sum(epoch_acc["train"]) / len(epoch_acc["train"]))
+                        epoch_loss["val"].append(loss)
 
-            print(
-                f"{epoch+1}/{epochs} -- Train Loss: {losses['train'][-1]} -- Train acc: {acc['train'][-1]} -- Val Loss: {losses['val'][-1]} -- Val acc: {acc['val'][-1]}"
-            )
+                        if self.mode == "classification":
+                            a = accuracy(pred, label)
+                            epoch_acc["val"].append(a)
+
+                        elif self.mode == "detection":
+                            a = accuracy(pred[1:5], label[1:5])
+                            epoch_acc["val"].append(a)
+
+                losses["val"].append(sum(epoch_loss["val"]) / len(epoch_loss["val"]))
+
+                if flag:
+
+                    acc["val"].append(sum(epoch_acc["val"]) / len(epoch_acc["val"]))
+                    acc["train"].append(
+                        sum(epoch_acc["train"]) / len(epoch_acc["train"])
+                    )
+
+                    print(
+                        f"{epoch+1}/{self.epochs} -- Train Loss: {losses['train'][-1]} -- Train acc: {acc['train'][-1]} -- Val Loss: {losses['val'][-1]} -- Val acc: {acc['val'][-1]}"
+                    )
+                else:
+                    print(
+                        f"{epoch+1}/{self.epochs} -- Train Loss: {losses['train'][-1]} -- Val Loss: {losses['val'][-1]}"
+                    )
+
+            else:
+
+                if flag:
+                    acc["train"].append(
+                        sum(epoch_acc["train"]) / len(epoch_acc["train"])
+                    )
+
+                    print(
+                        f"{epoch+1}/{self.epochs} -- Train Loss: {losses['train'][-1]} -- Train acc: {acc['train'][-1]}"
+                    )
+                else:
+                    print(
+                        f"{epoch+1}/{self.epochs} -- Train Loss: {losses['train'][-1]}"
+                    )
+
+            torch.save(self.model, f"{self.model_save_path}\\model")
+
+        if flag:
+            return losses, acc
 
         else:
-            print(
-                f"{epoch+1}/{epochs} -- Train Loss: {losses['train'][-1]} -- Val Loss: {losses['val'][-1]}"
-            )
+            return losses
 
-        torch.save(model, f"{model_save_path}\\model")
+    def test_sample(self, image, label=None):
 
-    if mode == "classification" or mode == "detection":
-        return losses, acc
+        pred = self.model(image)
 
-    else:
-        return losses
+        if label != None:
+            loss = self.loss_fn(label, pred).detach()
+            return pred, loss
 
+        return pred
 
-def test_sample(model, image, label=None, loss_fn=nn.MSELoss()):
+    def evaluate(self, test_path):
 
-    pred = model(image)
+        testset = ImageDataset(test_path, self.mode, device)
+        losses = []
 
-    if label != None:
-        loss = loss_fn(label, pred).detach()
-        return pred, loss
+        for img, label in testset:
+            pred = self.model(img.unsqueeze(0))
+            loss = self.loss_fn(label, pred).detach()
+            losses.append(loss)
 
-    return pred
+        return sum(losses) / len(losses)
+
+    def find_lr(self):
+        return 0.0
+
