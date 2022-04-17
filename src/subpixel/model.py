f0272c0bc08d90f970fab66770153cb2b07569b2
Rohit Rajesh <rajesh.rohit04@gmail.com>
2022-04-17 21:36:28 +0530

    indent

--- src/subpixel/model.py
+++ src/subpixel/model.py
@@ -7,12 +7,15 @@ from torchinfo import summary
 class Model(nn.Module):
     def __init__(self,path = 'arch.json') -> None:
         super(Model,self).__init__()
+
         JSON_file = open(path,"r")
         arch = json.load(JSON_file)
         JSON_file.close()
+        
         self.details = arch["details"]
         self.arch = arch["arch"]
         self.architecture = nn.ModuleList()
+
         for i in range(len(self.arch)):
             if self.arch[i]["code"] != i:
                 
@@ -21,17 +24,25 @@ class Model(nn.Module):
                 x = self.arch[i]["layer"]
                 y = eval(x)
                 self.architecture.append(y)
+        
         summary(self,self.details["input_shape"])
+    
     def forward(self,*X):
+
         outputs = []
+        
         if len(X)!= self.details["num_inputs"]:
+
             l  = self.details["num_inputs"]
             raise RuntimeError(f"Expected {l} inputs, got {len(X)}.")
+            
             return
 
         for i in range(len(self.arch)):
+
             if len(self.arch[i]["inputs"])==1:
                 outputs.append(self.architecture[i](outputs[self.arch[i]["inputs"][0]] if self.arch[i]["inputs"][0]>=0 else X[abs(self.arch[i]["inputs"][0])-1]))
+            
             elif len(self.arch[i]["inputs"])>=1:
                 x = torch.cat([outputs[j] if j>=0 else X[abs(j)-1] for j in self.arch[i]["inputs"]], self.arch[i]["cat_dim"])
                 outputs.append(self.architecture[i](x))
