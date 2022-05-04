5a40fe7b3effbbd231df8d64b5b19e0c2f6a4a12
Rohit Rajesh <rajesh.rohit04@gmail.com>
2022-05-04 17:35:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name="subpixel",
     version="0.0.1",
-    author="Rohit Rajesh",
-    author_email="rajesh.rohit04@gmail.com",
+    author="Rohit Rajesh, Adithya M",
+    author_email="rajesh.rohit04@gmail.com, adithyamanjunatha01@gmail.com",
     description="Train Computer Vision models on your custom dataset with just a few lines of code.",
     long_description=long_description,
     long_description_content_type="text/markdown",
