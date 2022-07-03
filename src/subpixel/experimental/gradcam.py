1c6a3898fd61e59e252c33891e7050cf6e047e8e
Rohit R <rajesh.rohit04@gmail.com>
2022-07-03 16:13:56 +0530

    .

--- src/subpixel/experimental/gradcam.py
+++ src/subpixel/experimental/gradcam.py
@@ -21,13 +21,13 @@ def get_activationMap(model, image, device='cpu'):
 
 def get_bbox(activation_maps, image_shape= None, threshold=0.5):
     
-        if image_shape:
-            activation_maps = cv2.resize(activation_maps, image_shape)
-    
-        activation_map = activation_maps[0]
-        activation_map = activation_map > threshold
-        contours, _ = cv2.findContours(activation_map.astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    
-        bbox = cv2.boundingRect(contours[0])
-    
-        return bbox
+    if image_shape:
+        activation_maps = cv2.resize(activation_maps, image_shape)
+
+    activation_map = activation_maps[0]
+    activation_map = activation_map > threshold
+    contours, _ = cv2.findContours(activation_map.astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    bbox = cv2.boundingRect(contours[0])
+
+    return bbox
